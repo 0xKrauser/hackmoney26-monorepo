23b916c23ac0debe84658b4ca99bb933a319f380
Johannes Krauser III <118311056+0xKrauser@users.noreply.github.com>
2026-02-01 15:01:38 +0200

    chore: update package versions to 0.0.1

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -47,23 +47,11 @@ const manifest = {
   content_scripts: [
     {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content/all.iife.js'],
-    },
-    {
-      matches: ['https://example.com/*'],
-      js: ['content/example.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content-ui/all.iife.js'],
-    },
-    {
-      matches: ['https://example.com/*'],
-      js: ['content-ui/example.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
       css: ['content.css'],
+    },
+    {
+      matches: ['https://x.com/*', 'https://www.x.com/*'],
+      js: ['inline-reactions/all.iife.js'],
     },
   ],
   web_accessible_resources: [
